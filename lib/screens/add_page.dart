eb1cb8ef7fc66218baddc94f24b9328d71a8078f
DulakshiAmarasooriya <dulakshidilhani8@gmail>
2023-03-10 14:28:14 +0530

    The 'Create' completed

---
+++ lib/screens/add_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'dart:convert';
+
+class AddTodoPage extends StatefulWidget {
+  const AddTodoPage({super.key});
+
+  @override
+  State<AddTodoPage> createState() => _AddTodoPageState();
+}
+
+class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Todo'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          TextField(
+            controller: titleController,
+            decoration: InputDecoration(hintText: 'Title'),
+          ),
+          TextField(
+            controller: descriptionController,
+            decoration: InputDecoration(hintText: 'Description'),
+            keyboardType: TextInputType.multiline,
+            minLines: 5,
+            maxLines: 8,
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: submitData,
+            child: const Text('Submit'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> submitData() async {
+    //Get the data from form
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      {
+        "title": title,
+        "description": description,
+        "is_completed": false,
+      }
+    };
+
+    //submit data to the server
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+
+    //show success or fail message on status
+    print(response.statusCode);
+    print(response.body);
+  }
+}
---
+++ lib/screens/todo_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'add_page.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: navigateToAddPage,
+        label: Text('Add Todo'),
+      ),
+    );
+  }
+
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(
+      builder: (contetx) => AddTodoPage(),
+    );
+    Navigator.push(context, route);
+  }
+}
